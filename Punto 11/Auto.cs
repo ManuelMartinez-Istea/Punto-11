f47f8bfefbe0f042f6aeb8dce72fca88a9ddd79d
Manuel Martinez <manuel.martinez@presidencia.gob.ar>
2023-05-22 13:22:55 -0300

    Agregar archivos de proyecto.

---
+++ Punto 11/Auto.cs
@@ -0,0 +1,15 @@
+﻿namespace Punto_11
+{
+    public class Auto
+    {
+       public Auto(string marca, string modelo, int anno) { 
+        this.Marca = marca;
+        this.Modelo = modelo; 
+        this.Anno = anno;
+        }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anno { get; set; }
+    }
+}
---
+++ Punto 11/Controllers/AutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Punto_11.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutoController : Controller
+    {
+        private static readonly Auto[] autos = new Auto[]
+        {
+            new Auto("Volkswagen", "Gol", 2010),
+            new Auto("Peugeot", "208", 2016),
+            new Auto("Renault", "Logan", 2014)
+       
+        };
+    
+
+        [HttpGet]
+        public IEnumerable<Auto> Get()
+        {
+            return autos;
+        }
+    }
+}
